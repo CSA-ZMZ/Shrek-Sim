f7ce85703d7cb7be8380cfd65e59d477fde9f81e
CSA-ZMZ <33459840+CSA-ZMZ@users.noreply.github.com>
2018-05-25 09:57:26 -0500

    Shrek Sim
    Shrek Sim is a fun text based game, inspired by the movie Shrek by DreamWorks Productions. You can answer Shrek trivia, play games, and role play.

--- Shrek Sim.py
+++ Shrek Sim.py
@@ -10,6 +10,22 @@
 #I have one audio file of the Shrek song All Star by Smash mouth. Shrek gave smash mouth the opprotunity to become a great band through Dreamwork's masterpeice of a movie.
 #Audio does not loop it stops at the end of the song.
 #There are no errors!
+
+"""
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import time
 points=0
 import random
